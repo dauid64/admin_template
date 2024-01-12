d10c4be2384db093acd6f7f27abe865f3356fe6c
dauid64 <carlosdauid64br@gmail.com>
2024-01-12 12:20:42 -0300

    add: componente menu lateral

---
+++ src/components/template/MenuItem.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+
+interface MenuItemProps {
+    url: string
+    text: string
+    icon: any
+}
+
+export default function MenuItem(props: MenuItemProps) {
+    return (
+        <li className="hover:bg-gray-100">
+            <Link href={props.url}>
+                <div className="flex flex-col justify-center items-center h-20 w-20">
+                    {props.icon}
+                    <span className="text-xs font-light text-gray-600">
+                        {props.text}
+                    </span>
+                </div>
+            </Link>
+        </li>
+    )
+}
--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,7 +1,14 @@
+import { BellIcon, HomeIcon, SettingsIcon } from "../icons"
+import MenuItem from "./MenuItem"
+
 export default function SideMenu() {
     return (
         <aside>
-            <h1>Menu</h1>
+            <ul>
+                <MenuItem url="/" text="Início" icon={HomeIcon}/>
+                <MenuItem url="/ajustes" text="Ajustes" icon={SettingsIcon}/>
+                <MenuItem url="/notificacoes" text="Notificações" icon={BellIcon}/>
+            </ul>
         </aside>
     )
 }
