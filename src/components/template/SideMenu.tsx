b85853687d03c50596a7c250cb413bd8fc7c2824
dauid64 <carlosdauid64br@gmail.com>
2024-01-15 11:27:03 -0300

    add: componente logo

--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,9 +1,17 @@
 import { BellIcon, HomeIcon, SettingsIcon } from "../icons"
+import Logo from "./Logo"
 import MenuItem from "./MenuItem"
 
 export default function SideMenu() {
     return (
         <aside>
+            <div className={`
+                flex flex-col items-center justify-center
+                bg-gradient-to-r from-indigo-500 to-purple-800
+                h-20 w-20
+            `}>
+                <Logo/>
+            </div>
             <ul>
                 <MenuItem url="/" text="Início" icon={HomeIcon}/>
                 <MenuItem url="/ajustes" text="Ajustes" icon={SettingsIcon}/>
