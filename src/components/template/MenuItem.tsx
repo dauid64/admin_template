c93beff8664dec9fe96337ce5ede67961370cd6d
dauid64 <carlosdauid64br@gmail.com>
2024-01-16 00:25:56 -0300

    fix: componente menu lateral

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,22 +1,36 @@
 import Link from "next/link"
 
 interface MenuItemProps {
-    url: string
+    url?: string
     text: string
     icon: any
+    className?: string
+    onClick?: (evento: any) => void
 }
 
 export default function MenuItem(props: MenuItemProps) {
+    function renderizarConteudo() {
+        return (
+            <div className={`flex flex-col justify-center items-center h-20 w-20 text-gray-600 ${props.className}`}>
+                {props.icon}
+                <span className="text-xs font-ligh">
+                    {props.text}
+                </span>
+            </div>
+        )
+    }
     return (
-        <li className="hover:bg-gray-100">
-            <Link href={props.url}>
-                <div className="flex flex-col justify-center items-center h-20 w-20">
-                    {props.icon}
-                    <span className="text-xs font-light text-gray-600">
-                        {props.text}
-                    </span>
-                </div>
-            </Link>
+        <li onClick={props.onClick} className="hover:bg-gray-100 cursor-pointer">
+            {
+                props.url ? (
+                    <Link href={props.url}>
+                        {renderizarConteudo()}
+                    </Link>
+                ) : (
+                    renderizarConteudo()
+                )
+            }
+            
         </li>
     )
 }
--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,10 +1,10 @@
-import { BellIcon, HomeIcon, SettingsIcon } from "../icons"
+import { BellIcon, HomeIcon, LogoutIcon, SettingsIcon } from "../icons"
 import Logo from "./Logo"
 import MenuItem from "./MenuItem"
 
 export default function SideMenu() {
     return (
-        <aside>
+        <aside className="flex flex-col">
             <div className={`
                 flex flex-col items-center justify-center
                 bg-gradient-to-r from-indigo-500 to-purple-800
@@ -12,10 +12,16 @@ export default function SideMenu() {
             `}>
                 <Logo/>
             </div>
-            <ul>
+            <ul className="flex-grow">
                 <MenuItem url="/" text="Início" icon={HomeIcon}/>
                 <MenuItem url="/ajustes" text="Ajustes" icon={SettingsIcon}/>
                 <MenuItem url="/notificacoes" text="Notificações" icon={BellIcon}/>
+            </ul>
+            <ul>
+                <MenuItem text="Sair" icon={LogoutIcon}
+                    onClick={() => console.log('logout')}
+                    className="text-red-600 hover:bg-red-400 hover:text-white"    
+                />
             </ul>
         </aside>
     )
