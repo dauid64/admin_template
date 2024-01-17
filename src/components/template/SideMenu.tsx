5dc1d7f943b7efd4dd14912154aa0c84ee9c92a8
dauid64 <carlosdauid64br@gmail.com>
2024-01-16 21:49:36 -0300

    add: esquema de cores

--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -4,7 +4,7 @@ import MenuItem from "./MenuItem"
 
 export default function SideMenu() {
     return (
-        <aside className="flex flex-col">
+        <aside className="flex flex-col bg-gray-200 text-gray-700  dark:bg-gray-900 ">
             <div className={`
                 flex flex-col items-center justify-center
                 bg-gradient-to-r from-indigo-500 to-purple-800
@@ -20,7 +20,7 @@ export default function SideMenu() {
             <ul>
                 <MenuItem text="Sair" icon={LogoutIcon}
                     onClick={() => console.log('logout')}
-                    className="text-red-600 hover:bg-red-400 hover:text-white"    
+                    className="text-red-600 dark:text-red-400 hover:bg-red-400 dark:hover:text-white hover:text-white"    
                 />
             </ul>
         </aside>
