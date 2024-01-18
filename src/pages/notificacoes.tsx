8b423536816b4e404d2e3346a9befacdfff4b6a8
dauid64 <carlosdauid64br@gmail.com>
2024-01-18 00:40:05 -0300

    fix: adicionando tela de autenticação

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,12 +1,9 @@
 import Layout from "../components/template/Layout"
-import { AppConsumer } from "../data/context/AppContext"
-import useAppData from "../data/hook/useAppData"
 
 export default function Notifications() {
-  const ctx = useAppData()
   return (
     <Layout title="Notificações" subtitle="Aqui você irá gerenciar as suas notificações">
-      <button onClick={ctx.alternarTema}>Alternar Tema</button>
+      <h1>Notificação</h1>
     </Layout>
   )
 }
