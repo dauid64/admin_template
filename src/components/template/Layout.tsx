cf78ffd7cfb4b2b3fb6bff70a4952928c852bdce
dauid64 <carlosdauid64br@gmail.com>
2024-01-21 19:29:38 -0300

    fix: protegendo rotas com função

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -2,7 +2,8 @@ import SideMenu from "./SideMenu"
 import Header from "./Header"
 import Content from "./Content"
 import useAppData from "@/src/data/hook/useAppData"
-import ForcarAutenticacao from "../auth/ForcarAutenticacao"
+import forcarAutenticacao from "@/functions/forcarAutenticacao"
+// import ForcarAutenticacao from "../auth/ForcarAutenticacao"
 
 interface LayoutProps {
     title: string
@@ -13,17 +14,15 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
     const ctx = useAppData()
 
-    return(
-        <ForcarAutenticacao>
-            <div className={`${ctx.tema} flex h-screen w-screen`}>
-                <SideMenu/>
-                <div className="flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800">
-                    <Header title={props.title} subtitle={props.subtitle}/>
-                    <Content>
-                        {props.children}
-                    </Content>
-                </div>
+    return forcarAutenticacao(
+        <div className={`${ctx.tema} flex h-screen w-screen`}>
+            <SideMenu/>
+            <div className="flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800">
+                <Header title={props.title} subtitle={props.subtitle}/>
+                <Content>
+                    {props.children}
+                </Content>
             </div>
-        </ForcarAutenticacao>
+        </div>
     )
 }
